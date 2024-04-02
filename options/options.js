d543f5e89b46f3b7eb956fc18b8a3a8a85a73af9
Crowdbotics <engineering@crowdbotics.com>
2024-04-02 11:01:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-butterfly-de-134726",
+  url: "https://lucky-butterfly-de-134726.botics.co",
+  api: "https://lucky-butterfly-de-134726.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
